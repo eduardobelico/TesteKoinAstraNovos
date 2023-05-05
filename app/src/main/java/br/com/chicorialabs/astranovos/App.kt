42b2e37fd613b9c6be2b1d5667d7e46c4117a289
Eduardo Bélico <116230274+eduardobelico@users.noreply.github.com>
2023-05-05 19:22:02 -0300

    Liberada a permissao de acesso a Internet no manifesto. Criado package para injecao de dependencias da presentation. No presentationModule, criadas as funcoes de injetar e carregar os modulos da viewModel. Criada classe App e introduzida no Manifesto como ponto de entrada do app e que inicia o Koin e carrega os modulos. Refatorando o HomeFragment  para receber o viewModel por injecao de dependencia.

---
+++ app/src/main/java/br/com/chicorialabs/astranovos/App.kt
@@ -0,0 +1,28 @@
+package br.com.chicorialabs.astranovos
+
+import android.app.Application
+import br.com.chicorialabs.astranovos.presentation.di.PresentationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+/**
+ * Essa classe representa o ponto de entrada no aplicativo e será a primeira classe executada.
+ */
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+        }
+
+        /**
+         * Carregar os módulos
+         */
+
+        PresentationModule.load()
+    }
+
+}
---
+++ app/src/main/java/br/com/chicorialabs/astranovos/presentation/di/PresentationModule.kt
@@ -0,0 +1,23 @@
+package br.com.chicorialabs.astranovos.presentation.di
+
+import br.com.chicorialabs.astranovos.presentation.ui.home.HomeViewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+/**
+ * Arquivo que organiza as dependências da camada Presentation
+ */
+
+object PresentationModule {
+
+    fun load() {
+        loadKoinModules(viewModelModule())
+    }
+
+    private fun viewModelModule(): Module {
+        return module {
+            factory { HomeViewModel()}
+        }
+    }
+}
